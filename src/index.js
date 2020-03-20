98ba03cb0084387dabc0c4a17e13ad4091510ed8
lfj <liao_fengji@163.com>
2020-03-20 17:43:20 +0800

    初始化项目

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import "nprogress/nprogress.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Router from "./router";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+import store from "./store";
+
+import ReactFastclick from "react-fastclick";
+
+ReactFastclick(); // 解决IOS几点不生效问题
+
+const App = (
+  <Provider store={store}>
+    <div>
+        <Router/>
+    </div>
+  </Provider>
+);
+
+ReactDOM.render(App, document.getElementById("root"));
+
 serviceWorker.unregister();
